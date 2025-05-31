a5be6d67d790e3f5e59c39631ccc50767726c5cf
Riccardo Alfonso Cerrone <riccardo.alfonsocer@outlook.it>
2025-05-31 19:27:58 +0200

    Added NoPeople at inizialization phase

--- dbPackage/dbInitialization.py
+++ dbPackage/dbInitialization.py
@@ -9,9 +9,9 @@ def run():
           dbTicketTypes.clearTicketTypes()
           
           print("Starting intialization: DAYS")
-          dbDays.insertDay("Venerdì", "2025-07-18")
-          dbDays.insertDay("Sabato", "2025-07-19")
-          dbDays.insertDay("Domenica", "2025-07-20")
+          dbDays.insertDay("Venerdì", "2025-07-18", 300)
+          dbDays.insertDay("Sabato", "2025-07-19", 0)
+          dbDays.insertDay("Domenica", "2025-07-20", 0)
           
           print("Starting initialization: STAGES")
           dbStages.insertStage("Palco A", "Main Stage")
@@ -19,7 +19,7 @@ def run():
           dbStages.insertStage("Palco C", "Experimental Stage")
           
           print("Starting initialization: TICKET TYPES")
-          dbTicketTypes.insertTicketType("Biglietto Giornaliero", 1, "-Consente l’ingresso al festival per una singola giornata a scelta.-Ideale per chi vuole vivere l’esperienza in modo concentrato o seguire un artista specifico.", "35.00")
-          dbTicketTypes.insertTicketType("Pass 2 Giorni", 2, "-Permette l’accesso al festival per due giornate consecutive.-Una soluzione conveniente per chi desidera godersi buona parte della manifestazione, con un risparmio di €10 rispetto all’acquisto di due biglietti giornalieri.", "60.00")
-          dbTicketTypes.insertTicketType("Full Pass", 3, "-Garantisce l’ingresso per l’intera durata del festival.-La scelta migliore per vivere ogni momento senza limiti, con un risparmio di €25 rispetto all’acquisto di tre biglietti giornalieri.", "80.00")
+          dbTicketTypes.insertTicketType("Biglietto Giornaliero", 1, '<li>Consente l’ingresso al festival per una singola giornata a scelta.</li><li>Ideale per chi vuole vivere l’esperienza in modo concentrato o seguire un artista specifico.</li>', "35.00")
+          dbTicketTypes.insertTicketType("Pass 2 Giorni", 2, '<li>Permette l’accesso al festival per due giornate consecutive.</li><li>Una soluzione conveniente per chi desidera godersi buona parte della manifestazione, con un risparmio di €10 rispetto all’acquisto di due biglietti giornalieri.</li>', "60.00")
+          dbTicketTypes.insertTicketType("Full Pass", 3, '<li>Garantisce l’ingresso per l’intera durata del festival.</li><li>La scelta migliore per vivere ogni momento senza limiti, con un risparmio di €25 rispetto all’acquisto di tre biglietti giornalieri.</li>.', "80.00")
           
