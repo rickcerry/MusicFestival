ec33b09b78653cf9e7b33df85313b40aa917e24f
Riccardo Alfonso Cerrone <riccardo.alfonsocer@outlook.it>
2025-05-31 19:28:42 +0200

    Implementation with exception of insertUser

--- dbPackage/dbUsers.py
+++ dbPackage/dbUsers.py
@@ -4,17 +4,26 @@ import sqlite3
 # Designed for Users table 
 
 # Inserts a user
-def insertUser(name, surname, email, typeUser):
-    sql = "INSERT INTO Users (Name, Surname, EMail, Type) VALUES (?, ?, ?, ?)"
+def insertUser(name, surname, email, typeUser, password):
+    success = False
+    sql = "INSERT INTO Users (Name, Surname, EMail, Type, Password) VALUES (?, ?, ?, ?, ?)"
     
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
-    cursor.execute(sql, (name, surname, email, typeUser))
     
-    conn.commit()
-    
+    try:
+        cursor.execute(sql, (name, surname, email, typeUser, password))
+        conn.commit()
+        success = True
+    except Exception as e:
+        print('ERROR', str(e))
+        # if something goes wrong: rollback
+        conn.rollback()
+
     cursor.close()
     conn.close()
+
+    return success
 
 # Given an ID, returns the user
 def getUser(id):
@@ -24,6 +33,21 @@ def getUser(id):
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute(sql, (id,))
+    
+    user = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return user
+
+def getUserViaEmail(email):
+    sql = "SELECT * FROM Users WHERE Users.EMail = ?"
+
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute(sql, (email,))
     
     user = cursor.fetchone()
 
