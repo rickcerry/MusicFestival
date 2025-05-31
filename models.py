2f258b8de8a23f1299fcc930a0f2eb0c93111058
Riccardo Alfonso Cerrone <riccardo.alfonsocer@outlook.it>
2025-05-31 19:27:20 +0200

    Used "mode" instead of "typeUser"

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
-          def __init__(self, id, name, surname, email, typeUser, password):
+          def __init__(self, id, name, surname, email, mode, password):
                     self.id = id
                     self.name = name
                     self.surname = surname
                     self.email = email
-                    self.type = typeUser
+                    self.mode = mode
                     self.password = password
